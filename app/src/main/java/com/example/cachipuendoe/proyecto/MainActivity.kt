c23da239207ef6b903d79f4c096a6e1a77f24776
cachipuendoe <ecachipuendo@gmail.com>
2018-12-27 00:05:23 -0500

    FRAGMENT CORREGIDO

--- app/src/main/java/com/example/cachipuendoe/proyecto/MainActivity.kt
+++ app/src/main/java/com/example/cachipuendoe/proyecto/MainActivity.kt
@@ -11,47 +11,22 @@ class MainActivity : AppCompatActivity() {
 
     val manager = this.supportFragmentManager
 
-/*
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.nav_home -> {
-                principal()
-                Toast.makeText(this, "Pestaña Home", Toast.LENGTH_SHORT).show()
-                return@OnNavigationItemSelectedListener true
-            }
-
-            R.id.nav_register -> {
-                registro()
-                Toast.makeText(this, "Pestaña Registro", Toast.LENGTH_SHORT).show()
-                return@OnNavigationItemSelectedListener true
-            }
-
-            R.id.nav_ajustes -> {
-                ajustes()
-                Toast.makeText(this, "Pestaña Ajustes", Toast.LENGTH_SHORT).show()
-                return@OnNavigationItemSelectedListener true
-            }
-
-        }
-        false
-    }*/
-
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.nav_home -> {
-                principal()
+                message.setText("Home")
                 createFragmenteHome()
                 return@OnNavigationItemSelectedListener true
             }
 
             R.id.nav_register -> {
-                registro()
+                message.setText("Registro")
                 createFragmenteRegistro()
                 return@OnNavigationItemSelectedListener true
             }
 
             R.id.nav_ajustes -> {
-                ajustes()
+                message.setText("Ajustes")
                 createFragmenteAjustes()
                 return@OnNavigationItemSelectedListener true
             }
@@ -65,47 +40,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        main_nav.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 
-
-        fun principal(){
-            var intent = Intent (this,MainActivity::class.java)
-            this.startActivity(intent)
-        }
-
-        fun registro(){
-            var intent = Intent (this,RegistroActivity::class.java)
-            this.startActivity(intent)
-        }
-
-        fun ajustes(){
-            var intent = Intent (this,AjustesActivity::class.java)
-            this.startActivity(intent)
-        }
-
-    fun createFragmenteHome() {
+    fun createFragmenteHome()
+    {
         val trans = manager.beginTransaction()
         val fragment = FragmentHome()
-        trans.replace(R.id.fragmentholder, fragment)
+        trans.replace(R.id.fragmentholder,fragment)
         trans.addToBackStack(null)
         trans.commit()
     }
 
-    fun createFragmenteRegistro() {
+    fun createFragmenteRegistro()
+    {
         val trans = manager.beginTransaction()
         val fragment = FragmentRegistro()
-        trans.replace(R.id.fragmentholder, fragment)
+        trans.replace(R.id.fragmentholder,fragment)
         trans.addToBackStack(null)
         trans.commit()
     }
 
-    fun createFragmenteAjustes() {
+    fun createFragmenteAjustes()
+    {
         val trans = manager.beginTransaction()
         val fragment = FragmentAjustes()
-        trans.replace(R.id.fragmentholder, fragment)
+        trans.replace(R.id.fragmentholder,fragment)
         trans.addToBackStack(null)
         trans.commit()
     }
+
 
 }
